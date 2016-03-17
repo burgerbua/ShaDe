6deb9b164b90949deae5940ae180e79ec2df4f7f
Matthias Messner <burgerbua@gmail.com>
2016-03-17 17:05:35 -0600

    Initial Commit

---
+++ ShaDe/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ShaDe
+//
+//  Created by Matthias Messner on 3/17/16.
+//  Copyright © 2016 burgerbua. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
