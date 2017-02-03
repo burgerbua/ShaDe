19e2dd461502b0a41785d2df0e6bb4a6ace06064
Matthias Messner <burgerbua@gmail.com>
2017-02-02 18:41:51 -0700

    Update point.hxx

--- point.hxx
+++ point.hxx
@@ -10,68 +10,31 @@
 #define point_h
 
 namespace shade {
-	
-	// integer point
-	using point3i = std::array<uint32_t, 3>;
-
-	// double point
-	using point3r = std::array<double, 3>;
-}
-
-bool operator==(
-	const shade::point3i& l,
-	const shade::point3i& r)
-{
-	return l[0] == r[0] && l[1] == r[1] && l[2] == r[2];
-}
-
-template <typename point3>
-point3 operator-(
-	const point3& l,
-	const point3& r)
-{
-	return{ { l[0] - r[0], l[1] - r[1], l[2] - r[2] } };
-}
-template <typename point3>
-point3 operator+(
-	const point3& l,
-	const point3& r)
-{
-	return{ { l[0] + r[0], l[1] + r[1], l[2] + r[2] } };
-}
-template <typename point3>
-point3 operator*(
-	const point3& l,
-	const double val)
-{
-	return{ { l[0] * val, l[1] * val, l[2] * val } };
-}
-template <typename point3>
-double dot(
-	const point3& l,
-	const point3& r)
-{
-	return l[0] * r[0] + l[1] * r[1] + l[2] * r[2];
-}
-template <typename point3>
-point3 cross(
-	const point3& l,
-	const point3& r)
-{
-	return{ { l[1] * r[2] - l[2] * r[1], l[2] * r[0] - l[0] * r[2], l[0] * r[1] - l[1] * r[0] } };
-}
-template <typename point3>
-double norm(
-	const point3& v)
-{
-	return sqrt(dot(v, v));
-}
-template <typename point3>
-point3 normalize(
-	const point3& vec)
-{
-	const double len = norm(vec);
-	return{ { vec[0] / len, vec[1] / len, vec[2] / len } };
+    
+    typedef std::array<double, 3> point_type;
+    
+    inline point_type operator-(const point_type& l, const point_type& r) {
+        return {{l[0]-r[0], l[1]-r[1], l[2]-r[2]}};
+    }
+    inline point_type operator+(const point_type& l, const point_type& r) {
+        return {{l[0]+r[0], l[1]+r[1], l[2]+r[2]}};
+    }
+    inline point_type operator*(const point_type& l, const double val) {
+        return {{l[0]*val, l[1]*val, l[2]*val}};
+    }
+    inline double dot(const point_type& l, const point_type& r) {
+        return l[0]*r[0] + l[1]*r[1] + l[2]*r[2];
+    }
+    inline point_type cross(const point_type& l, const point_type& r) {
+        return {{l[1]*r[2] - l[2]*r[1], l[2]*r[0] - l[0]*r[2], l[0]*r[1] - l[1]*r[0]}};
+    }
+    inline double norm(const point_type& v) {
+        return sqrt(dot(v, v));
+    }
+    inline point_type normalize(const point_type& vec) {
+        const double len = norm(vec);
+        return {{vec[0]/len, vec[1]/len, vec[2]/len}};
+    }
 }
 
 #endif /* point_h */
